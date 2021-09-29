3d2977cbab775de62b1cb48b1eac4136c393d272
liuyuan <liuyuan@qq.com>
2021-09-29 08:38:02 +0800

    feat: 添加通过颜色进行图片识别的逻辑

---
+++ verifyCode/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from collections import Counter
+import numpy as np
+from sklearn.cluster import KMeans
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+clt = KMeans(n_clusters=7)
+def palette_perc(k_cluster):
+    width = 300
+    palette = np.zeros((50, width, 3), np.uint8)
+
+    n_pixels = len(k_cluster.labels_)
+    counter = Counter(k_cluster.labels_) # count how many pixels per cluster
+    perc = {}
+    for i in counter:
+        perc[i] = np.round(counter[i]/n_pixels, 2)
+    perc = dict(sorted(perc.items()))
+
+    #for logging purposes
+    print(perc)
+    print(k_cluster.cluster_centers_)
+
+    step = 0
+
+    for idx, centers in enumerate(k_cluster.cluster_centers_): 
+        palette[:, step:int(step + perc[idx]*width+1), :] = centers
+        step += int(perc[idx]*width+1)
+
+    return palette
+def show_img_compar(img_1, img_2):
+    f, ax = plt.subplots(1, 2, figsize=(10,10))
+    ax[0].imshow(img_1)
+    ax[1].imshow(img_2)
+    ax[0].axis('off')
+    ax[1].axis('off')
+    f.tight_layout()
+    plt.show()
+
+def palette(clusters):
+    width=300
+    palette = np.zeros((50, width, 3), np.uint8)
+    steps = width/clusters.cluster_centers_.shape[0]
+    for idx, centers in enumerate(clusters.cluster_centers_): 
+        palette[:, int(idx*steps):(int((idx+1)*steps)), :] = centers
+    return palette
+
+img=cv.imread('./1.jpeg')
+clt_1 = clt.fit(img.reshape(-1, 3))
+show_img_compar(img, palette_perc(clt_1))
+
+# clt_2 = clt.fit(img_2.reshape(-1, 3))
+# show_img_compar(img_2, palette(clt_2))
