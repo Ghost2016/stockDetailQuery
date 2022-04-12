42c7fdba271c7b4a9ecf743308329abfa0e5c7bb
Ghost2016 <454201948@qq.com>
2022-04-12 20:09:42 +0800

    fix: 打开注释内容

--- fetchRateAndHeight.py
+++ fetchRateAndHeight.py
@@ -44,103 +44,103 @@ def getCategories(day):
     theDayBeforeLastTradeDay = getLastTradeDay(lastTradeDay)
     threeDayBefore = getLastTradeDay(theDayBeforeLastTradeDay)
     return[
-    # {
-    #   'category': '%s曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, no_st + no_new),
-    #   'text': '首板炸',
-    #   'method': crawl_length
-    # },
-    # {
-    #   'category': '%s曾涨停 %s' % (currentDay, no_st + no_new),
-    #   'text': '涨停炸',
-    #   'method': crawl_length
-    # },
-    # {
-    #   'category': '%s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, no_st + no_new),
-    #   'text': '首板',
-    #   'method': crawl_length
-    # },
-    # getSpecificHeightData(2, currentDay),
-    # getSpecificHeightData(3, currentDay),
-    # getSpecificHeightData(4, currentDay),
-    # getSpecificHeightData(5, currentDay),
-    # {
-    #   'category': '%s涨停 %s连续涨停天数大于5 %s' % (currentDay, currentDay, no_st + no_new),
-    #   'text': '5板+',
-    #   'method': crawl_length
-    # },
-    # {
-    #   'text': '最高板',
-    #   'func': crawl_highest('非st 非创业板 非科创板 非新股 %s二连板以上' % (currentDay), currentDay)
-    # },
-    # autoCal('涨停数'),
-    # autoCal('连板数'),
-    # autoCal('整体封板率'),
-    # autoCal('首板成功率'),
-    # autoCal('1进2晋级率'),
-    # autoCal('2进3晋级率'),
-    # autoCal('3进4晋级率'),
-    # autoCal('4进5晋级率'),
-    # autoCal('5板以上晋级率'),
-    # autoCal('连板晋级率(2板+)'),
-    # autoCal('中高位晋级率(3板+)'),
-    # autoCal('总情绪'),
-    # {
-    #   'category': '%s跌停 %s' % (currentDay, no_st + no_new),
-    #   'text': '跌停',
-    #   'method': crawl_length
-    # },
-    # {
-    #   'text': '昨日首板成功收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日首板失败收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日首板真实收益率（包含炸板）',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日1进2成功收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s连续涨停天数为2 %s' % (currentDay, lastTradeDay, no_st + no_new), currentDay)
-    # },
+    {
+      'category': '%s曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, no_st + no_new),
+      'text': '首板炸',
+      'method': crawl_length
+    },
+    {
+      'category': '%s曾涨停 %s' % (currentDay, no_st + no_new),
+      'text': '涨停炸',
+      'method': crawl_length
+    },
+    {
+      'category': '%s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, no_st + no_new),
+      'text': '首板',
+      'method': crawl_length
+    },
+    getSpecificHeightData(2, currentDay),
+    getSpecificHeightData(3, currentDay),
+    getSpecificHeightData(4, currentDay),
+    getSpecificHeightData(5, currentDay),
+    {
+      'category': '%s涨停 %s连续涨停天数大于5 %s' % (currentDay, currentDay, no_st + no_new),
+      'text': '5板+',
+      'method': crawl_length
+    },
+    {
+      'text': '最高板',
+      'func': crawl_highest('非st 非创业板 非科创板 非新股 %s二连板以上' % (currentDay), currentDay)
+    },
+    autoCal('涨停数'),
+    autoCal('连板数'),
+    autoCal('整体封板率'),
+    autoCal('首板成功率'),
+    autoCal('1进2晋级率'),
+    autoCal('2进3晋级率'),
+    autoCal('3进4晋级率'),
+    autoCal('4进5晋级率'),
+    autoCal('5板以上晋级率'),
+    autoCal('连板晋级率(2板+)'),
+    autoCal('中高位晋级率(3板+)'),
+    autoCal('总情绪'),
+    {
+      'category': '%s跌停 %s' % (currentDay, no_st + no_new),
+      'text': '跌停',
+      'method': crawl_length
+    },
+    {
+      'text': '昨日首板成功收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日首板失败收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日首板真实收益率（包含炸板）',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或曾涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日1进2成功收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s连续涨停天数为2 %s' % (currentDay, lastTradeDay, no_st + no_new), currentDay)
+    },
     {
       'text': '昨日1进2失败收益率',
       'func': crawl_earning_of_stocks('%s涨跌幅 %s曾涨停 %s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, threeDayBefore, no_st + no_new), currentDay, True)
     },
-    # {
-    #   'text': '昨日1进2真实收益率（包含炸板）',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或者曾涨停 %s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, threeDayBefore, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日连板成功收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s连续涨停天数为2以上 %s' % (currentDay, lastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日连板失败收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s曾涨停 %s涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日连板真实收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或者曾涨停 %s涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '前日连板失败收益率',
-    #   'func': crawl_earning_of_stocks('%s涨跌幅 %s未涨停 %s涨停 %s' % (currentDay, theDayBeforeLastTradeDay, threeDayBefore, no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '当日破板平均亏损',
-    #   'func': crawl_lost_of_stocks('%s曾涨停 %s涨停价 %s收盘价 %s收盘价不为空 %s' % (currentDay, currentDay, currentDay, lastTradeDay,  no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日破板平均收益',
-    #   'func': crawl_earning_of_stocks('%s曾涨停 %s' % (lastTradeDay,  no_st + no_new), currentDay)
-    # },
-    # {
-    #   'text': '昨日所有涨停真实收益率（包含炸板）',
-    #   'func': crawl_earning_of_stocks('%s涨停或者曾涨停 %s' % (lastTradeDay,  no_st + no_new), currentDay)
-    # },
+    {
+      'text': '昨日1进2真实收益率（包含炸板）',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或者曾涨停 %s涨停 %s未涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, threeDayBefore, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日连板成功收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s连续涨停天数为2以上 %s' % (currentDay, lastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日连板失败收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s曾涨停 %s涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日连板真实收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s涨停或者曾涨停 %s涨停 %s' % (currentDay, lastTradeDay, theDayBeforeLastTradeDay, no_st + no_new), currentDay)
+    },
+    {
+      'text': '前日连板失败收益率',
+      'func': crawl_earning_of_stocks('%s涨跌幅 %s未涨停 %s涨停 %s' % (currentDay, theDayBeforeLastTradeDay, threeDayBefore, no_st + no_new), currentDay)
+    },
+    {
+      'text': '当日破板平均亏损',
+      'func': crawl_lost_of_stocks('%s曾涨停 %s涨停价 %s收盘价 %s收盘价不为空 %s' % (currentDay, currentDay, currentDay, lastTradeDay,  no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日破板平均收益',
+      'func': crawl_earning_of_stocks('%s曾涨停 %s' % (lastTradeDay,  no_st + no_new), currentDay)
+    },
+    {
+      'text': '昨日所有涨停真实收益率（包含炸板）',
+      'func': crawl_earning_of_stocks('%s涨停或者曾涨停 %s' % (lastTradeDay,  no_st + no_new), currentDay)
+    },
   ]
 
 # 获取数据
